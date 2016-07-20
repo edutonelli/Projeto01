928d7621992345791811d6a791a979091a18e389
tonelli <tonelli@TONELLI-PC>
2016-07-20 11:32:23 -0300

    Index Categoria

--- Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,19 @@ namespace Projeto01.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() {CategoriaId = 1, Nome = "Notebook" },
+            new Categoria() {CategoriaId = 2, Nome = "Impressora" },
+            new Categoria() {CategoriaId = 3, Nome = "Tablet" },
+            new Categoria() {CategoriaId = 4, Nome = "Cabo" },
+            new Categoria() {CategoriaId = 5, Nome = "bola" },
+        };
+
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
--- Projeto01/Models/Categoria.cs
+++ Projeto01/Models/Categoria.cs
@@ -7,5 +7,7 @@ namespace Projeto01.Models
 {
     public class Categoria
     {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
     }
 }
